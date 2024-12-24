aa4c5171898eeb1916bd31485d2c4a409be8efbb
bilal77511 <bilal77511@gmail.com>
2024-12-24 22:05:46 +0500

    portfolio deleted

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const typed = new Typed('.typed', {
-        strings: ['AI Engineer', 'Software Developer', 'Problem Solver'],
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
-});
